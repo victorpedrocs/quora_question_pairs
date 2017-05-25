327cb4fe8857da615f1a035bdafe974444eba5d3
vpcsilva <vpcx5@yahoo.com.br>
2017-05-24 21:21:45 -0300

    Changing the package of cross_val_score: Because of deprecation warning.

--- quora_classifier.py
+++ quora_classifier.py
@@ -1,16 +1,18 @@
+#%%
 from pandas import read_csv
 from numpy  import array
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import naive_bayes
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import cross_val_score
+from sklearn.model_selection import cross_val_score
 
-dataset = open('dataset_concat_stemm.csv', 'r')
+dataset = open('dataset_concat_stemm.csv' , 'r')
+#%%
 dataset = read_csv(dataset, header=None, delimiter='\t')
 
 print(dataset.shape)
-
+#%%
 x = dataset.iloc[:,0]
 y = array(dataset.iloc[:,1]).ravel()
 
@@ -19,7 +21,7 @@ x = vectorizer.fit_transform(x)
 
 print(x.shape)
 print(y.shape)
-
+#%%
 classifier = naive_bayes.MultinomialNB()
 
 # classifier = RandomForestClassifier(n_estimators=5, n_jobs=-1)
@@ -27,6 +29,4 @@ classifier = naive_bayes.MultinomialNB()
 scores = cross_val_score(classifier, x, y, cv=2, n_jobs=-1)
 
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-
-
+#%%
