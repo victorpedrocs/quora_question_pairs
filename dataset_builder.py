ade2f03ceb25555d8beac2e4f9ff18b78d16b0d9
vpcsilva <vpcx5@yahoo.com.br>
2017-05-30 12:08:32 -0300

    Changing preprocess and dataset_builder

--- dataset_builder.py
+++ dataset_builder.py
@@ -4,6 +4,7 @@ import csv
 from nltk.stem.snowball import EnglishStemmer
 from nltk.tokenize import RegexpTokenizer
 from contractions import *
+from sklearn.externals.joblib import Parallel, delayed
 
 tokenizer = RegexpTokenizer(r'\w+')
 stemmer = EnglishStemmer()
@@ -20,6 +21,49 @@ def tokenize(q):
 def stem(q):
     return ' '.join([stemmer.stem(word).lower() for word in tokenizer.tokenize(q)])
 
+def all_prep(text):
+    return stem(replace_contractions(text.strip()))
+
+def concat_row_prep(r, writer):
+    q1 = all_prep(r[1])
+    q2 = all_prep(r[2])
+    q = q1 + " " + q2
+    y = r[3]
+    writer.writerow([q,y])
+
+def read_file(path):
+    dataset = read_csv(path, header='infer', delimiter='\t')
+    dataset.drop(dataset.columns[[0,1,2]], axis=1, inplace=True)
+    dataset.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
+    return dataset
+
+def generate_concat():
+    dataset = read_file('./datasets/smallsample.csv')
+
+    out = open('./datasets/dataset_concat.csv', mode='w')
+    csv_writer = csv.writer(out, delimiter='\t')
+
+    for row in dataset.itertuples():
+        concat_row_prep(row, csv_writer)
+
+    out.close()
+
+def generate_one_question_per_line():
+    dataset = read_file('./datasets/smallsample.csv')
+
+    out_X = open('./datasets/dataset_singlequestion_X.csv', mode='w')
+    out_y = open('./datasets/dataset_singlequestion_y.csv', mode='w')
+    writer_X = csv.writer(out_X, delimiter='\t')
+    writer_y = csv.writer(out_y, delimiter='\t')
+
+    for r in dataset.itertuples():
+        writer_X.writerow([all_prep(r[1])])
+        writer_X.writerow([all_prep(r[2])])
+        writer_y.writerow([r[3]])
+
+    out_X.close()
+    out_y.close()
+
 def generate(out_type):
     # load the dataset into a dataframe
     dataset = read_csv('./datasets/dataset.csv', header='infer', delimiter='\t')
@@ -29,10 +73,10 @@ def generate(out_type):
 
     out = open('./datasets/dataset_'+out_type+'.csv', mode='w')
     csv_writer = csv.writer(out, delimiter='\t')
-    
+
     # remove non-ascii characters
     dataset.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
-        
+
     for row in dataset.itertuples():
         q1 = row[1].strip() # copy the tuple content to q1
         q2 = row[2].strip() # copy the tuple content to q2
--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,28 @@
+#!/Users/vpedro/PESC/2017.1/data_mining/assignments/quora/bin/python
 import sys, getopt
 import dataset_builder as dsb
+
+def print_help():
+    print("\t-s:\t\tSingle Question per line dataset")
+    print("\t-c:\t\tConcat question in one line")
 
 def main(argv):
     out_type = ''
     sample_size = ''
     try:
-        opts, args = getopt.getopt(argv, "ht:")
+        opts, args = getopt.getopt(argv, "hsc")
     except getopt.GetoptError:
-        print("-t [stemm, tokenize, simple]")
+        print_help()
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print("-t [stemm, tokenize, simple]")
+            print_help()
             sys.exit()
-        elif opt == '-t':
-            out_type = arg
-
-    dsb.generate(out_type)
+        elif opt == '-s':
+            dsb.generate_one_question_per_line()
+        elif opt == '-c':
+            dsb.generate_concat()
 
 
 
