282f5e24949ebc87109d2a4352231158647f0fcf
vpcsilva <vpcx5@yahoo.com.br>
2017-05-31 10:09:19 -0300

    Picking classifiers from lab on server

--- ngram.py
+++ ngram.py
@@ -1,23 +1,24 @@
-from numpy import load, ones, divide
+from numpy import load, ones, save
 from scipy.sparse import csr_matrix, hstack
+from sklearn.decomposition import TruncatedSVD
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.linear_model import LogisticRegression
-from numpy.linalg import norm
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, log_loss
 from sklearn.model_selection import train_test_split
 from joblib import Parallel, delayed
 
-X = load('./datasets/300d-1-x.npy')
+#X = csr_matrix(load('./datasets/count-stemm-12g.npy').all())
 #X = hstack((X, csr_matrix(ones((X.shape[0], 1)))))
+X = load('./datasets/count-stemm-12g-300tsvd.npy')
 y = load('./datasets/y.npy')
 
-out = open('./resultados/xgboost-word2vec.txt', 'w')
+#pca = TruncatedSVD(n_components=50)
+#X = pca.fit_transform(X)
+#save('./datasets/count-stemm-12g-50tsvd', X)
 
-for row in range(X.shape[0]):
-    X[row] = divide(X[row], norm(X[row]))
-    
+out = open('./resultados/xgboost-ngram.txt', 'w')
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=25)
 
 def train_model(X_train, y_train, _n_estimators):
@@ -32,7 +33,7 @@ for model in result:
     y_pred = model.predict(X_test)
     out.write(str(model) + '\n\n')
     out.write(str(classification_report(y_test, y_pred)) + '\n\n')
-    out.write("Acurracy: " + str(accuracy_score(y_test, y_pred)) + '\n\n')
+    out.write("Accuracy: " + str(accuracy_score(y_test, y_pred)) + '\n\n')
     out.write("Log Loss: " + str(log_loss(y_test, y_pred)) + '\n\n')
     out.write(str(confusion_matrix(y_test, y_pred)) + '\n\n')
 
@@ -42,7 +43,7 @@ for model in result:
 #score = str("Log Loss (RF): %f (+/- %f)" % (scores.mean(), scores.std() * 2))
 #out.write(score)
 
-#model = XGBClassifier(n_estimators=100)
+#model = XGBClassifier(n_estimators=1000, max_depth=7, base_score=0.2, subsample=0.6)
 #scores = cross_val_score(model, X, y, cv=5, n_jobs=-1, scoring='neg_log_loss')
 
 #score = str("Log Loss (XGB): %f (+/- %f)" % (scores.mean(), scores.std() * 2))
